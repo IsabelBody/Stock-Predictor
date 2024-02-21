64871b65c4f0c696b133091f64320ca9aa3038a7
Isabel Body <isabelbody@gmail.com>
2024-02-21 14:49:16 +1300

    host change

--- main.py
+++ main.py
@@ -16,4 +16,4 @@ def display_predictions():
     return render_template('index.html', predictions=predictions)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000)
